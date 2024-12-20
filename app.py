ede334174ea7cb40325ef15aa3e59254f5497004
Tyler Collins <tylercollins@Tylers-MacBook-Pro.local>
2024-12-19 21:37:15 -0800

    add dynamo db functionality

--- app.py
+++ app.py
@@ -2,16 +2,20 @@
 # SPDX-License-Identifier: MIT-0
 
 from flask import Flask, jsonify, request, render_template
-from pprint import pprint
 import boto3
-from botocore.exceptions import ClientError
 import os
 from twilio.twiml.messaging_response import MessagingResponse
+
+from interactions import Interactions
 
 app = Flask(__name__)
 
 dynamodb = boto3.resource('dynamodb',region_name=os.environ['AWS_REGION'])
 table = dynamodb.Table(os.environ['DDB_TABLE'])
+interactions = Interactions(dynamodb, logger=app.logger)
+
+if not interactions.exists(table):
+  interactions.create_table(table)
 
 
 #Home Page
@@ -23,20 +27,56 @@ def home():
 # POST /api/whatsapp
 @app.route('/api/whatsapp', methods=['POST'])
 def whatsapp_reply():
+  # Record timestamp message was received
+  timestamp = datetime.strftime(datetime.now(), '%m-%d-%y %H:%M:%S')
+
+  # Attempt to extract the phone number
   try:
-    body = request.values.get('Body', None)
+    phone = request.values.get('From', None)
   except:
-    body = None
+    phone = None
+
+  # Attempt to extract the message body
+  try:
+    received_message = request.values.get('Body', None)
+  except:
+    received_message = None
 
   resp = MessagingResponse()
 
-  if body is not None:
-    resp.message(f"You sent: '{body}'")
+  app.logger.error(f"phone={phone}, received_message={received_message}")
+
+  if phone is not None and received_message is not None:
+    # Define name variable
+    name = ""
+
+    # Get records of previous interactions and tabulate count
+    previous_interaction_records = interactions.query_interactions(phone)
+    previous_interaction_count = len(previous_interaction_records)
+
+    # Create a message to send based on combination of message received and the count of interactions
+    message_to_send = f'This is your #{previous_interaction_count + 1} interaction, you sent the message "{received_message}"'
+
+    # Incorporate message into response
+    resp.message(message_to_send)
+
   else:
-    resp.message("Body not parsed successfully")
+    name = ""
+    if phone is None and received_message is not None:
+      message_to_send = 'Phone not parsed successfully'
+    elif phone is not None and received_message is None:
+      message_to_send = 'Body not parsed successfully'
+    else:
+      message_to_send = 'Phone and body not parsed successfully'
+
+    resp.message(message_to_send)
+
+  # Write record to dynamodb
+  interactions.add_interaction(phone, timestamp, name, received_message, message_to_send)
+
 
   return str(resp)
 
 
-if __name__ == "__main__":
-   app.run(host="0.0.0.0", port=8080)
+if __name__ == '__main__':
+   app.run(host='0.0.0.0', port=8080)
---
+++ interactions.py
@@ -0,0 +1,209 @@
+from datetime import datetime
+from boto3.dynamodb.conditions import Key
+from botocore.exceptions import ClientError
+
+logger = logging.getLogger(__name__)
+
+class Interactions:
+    """Encapsulates an Amazon DynamoDB table of interactions.
+
+    Example data structure for a movie record in this table:
+        {
+            "phone": "+13238745",
+            "timestamp": "'12-19-24 20:43:36",
+            "name": "Amanda",
+            "received_message": "Hello World!",
+            "sent_message": "And hello to you too"
+        }
+    """
+
+    def __init__(self, dyn_resource, logger=None):
+        """
+        :param dyn_resource: A Boto3 DynamoDB resource.
+        """
+        self.dyn_resource = dyn_resource
+        self.logger = logger
+        # The table variable is set during the scenario in the call to
+        # 'exists' if the table exists. Otherwise, it is set by 'create_table'.
+        self.table = None
+
+
+    def exists(self, table_name):
+        """
+        Determines whether a table exists. As a side effect, stores the table in
+        a member variable.
+
+        :param table_name: The name of the table to check.
+        :return: True when the table exists; otherwise, False.
+        """
+        try:
+            table = self.dyn_resource.Table(table_name)
+            table.load()
+            exists = True
+        except ClientError as err:
+            if err.response["Error"]["Code"] == "ResourceNotFoundException":
+                exists = False
+            else:
+                self.logger.error(
+                    "Couldn't check for existence of %s. Here's why: %s: %s",
+                    table_name,
+                    err.response["Error"]["Code"],
+                    err.response["Error"]["Message"],
+                )
+                raise
+        else:
+            self.table = table
+        return exists
+
+
+    def create_table(self, table_name):
+        """
+        Creates an Amazon DynamoDB table that can be used to store interaction data.
+        The table uses the phone the partition key and the
+        timestamp as the sort key.
+
+        :param table_name: The name of the table to create.
+        :return: The newly created table.
+        """
+        try:
+            self.table = self.dyn_resource.create_table(
+                TableName=table_name,
+                KeySchema=[
+                    {"AttributeName": "phone", "KeyType": "HASH"},  # Partition key
+                    {"AttributeName": "timestamp", "KeyType": "RANGE"},  # Sort key
+                ],
+                AttributeDefinitions=[
+                    {"AttributeName": "phone", "AttributeType": "S"},
+                    {"AttributeName": "timestamp", "AttributeType": "S"},
+                ],
+                ProvisionedThroughput={
+                    "ReadCapacityUnits": 10,
+                    "WriteCapacityUnits": 10,
+                },
+            )
+            self.table.wait_until_exists()
+        except ClientError as err:
+            self.logger.error(
+                "Couldn't create table %s. Here's why: %s: %s",
+                table_name,
+                err.response["Error"]["Code"],
+                err.response["Error"]["Message"],
+            )
+            raise
+        else:
+            return self.table
+
+
+    def list_tables(self):
+        """
+        Lists the Amazon DynamoDB tables for the current account.
+
+        :return: The list of tables.
+        """
+        try:
+            tables = []
+            for table in self.dyn_resource.tables.all():
+                print(table.name)
+                tables.append(table)
+        except ClientError as err:
+            self.logger.error(
+                "Couldn't list tables. Here's why: %s: %s",
+                err.response["Error"]["Code"],
+                err.response["Error"]["Message"],
+            )
+            raise
+        else:
+            return tables
+
+
+    def add_interaction(self, phone, timestamp, name, received_message, sent_message):
+        """
+        Adds an interaction to the table.
+
+        :param phone: The user's phone number
+        :param timestamp: Timestamp when the user's message was received by the server
+        :param name: The user's name
+        :param received_message: Content of the received message
+        :param sent_message: Content of the message server send back to user
+        """
+        try:
+            self.table.put_item(
+                Item={
+                    "phone": phone,
+                    "timestamp": timestamp,
+                    "name": name,
+                    "received_message": received_message,
+                    "sent_message": sent_message
+                }
+            )
+        except ClientError as err:
+            self.logger.error(
+                "Couldn't add interaction phone %s, timestamp %s to table %s. Here's why: %s: %s",
+                phone,
+                timestamp,
+                self.table.name,
+                err.response["Error"]["Code"],
+                err.response["Error"]["Message"],
+            )
+            raise
+
+
+    def query_interactions(self, phone):
+        """
+        Queries for interactions with the specified phone.
+
+        :param phone: The phone to query.
+        :return: The list of interactions with the specified phone.
+        """
+        try:
+            response = self.table.query(KeyConditionExpression=Key("phone").eq(phone))
+        except ClientError as err:
+            self.logger.error(
+                "Couldn't query for interactions with phone %s. Here's why: %s: %s",
+                phone,
+                err.response["Error"]["Code"],
+                err.response["Error"]["Message"],
+            )
+            raise
+        else:
+            return response["Items"]
+
+
+    # def delete_interactions(self, title, year):
+    # TO BE DONE
+    #     """
+    #     Deletes a movie from the table.
+
+    #     :param title: The title of the movie to delete.
+    #     :param year: The release year of the movie to delete.
+    #     """
+    #     try:
+    #         self.table.delete_item(Key={"year": year, "title": title})
+    #     except ClientError as err:
+    #         self.logger.error(
+    #             "Couldn't delete movie %s. Here's why: %s: %s",
+    #             title,
+    #             err.response["Error"]["Code"],
+    #             err.response["Error"]["Message"],
+    #         )
+    #         raise
+
+
+    def delete_table(self):
+        """
+        Deletes the table.
+        """
+        try:
+            self.table.delete()
+            self.table = None
+        except ClientError as err:
+            self.logger.error(
+                "Couldn't delete table. Here's why: %s: %s",
+                err.response["Error"]["Code"],
+                err.response["Error"]["Message"],
+            )
+            raise
+
+
+
+
